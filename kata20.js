b5c3b7e9ce8dd897dea7b3ae8d63578667ef9cb1
Marta Castrillo <martacastrillo@MacBook-Pro-de-Marta.local>
2023-01-31 10:03:53 +0100

    add kata 20

---
+++ kata20.js
@@ -0,0 +1,33 @@
+/* En este desafío vas a recibir el título de un artículo y tú debes transformarlo en un formato de URL en donde normalmente se transforma todo en minúscula y se cambian los espacios por un guion (-).
+Para solucionarlo vas a encontrar una función llamada parseToURL que recibe un parámetro de entrada:
+
+title: Un String con el título.
+Dentro del cuerpo de la función parseToURL debes escribir tu solución.
+
+Ejemplo 1:
+Input: "La forma de correr Python"
+Output: "la-forma-de-correr-python"
+
+Ejemplo 2:
+Input: "La API para nunca parar de aprender"
+Output: "la-api-para-nunca-parar-de-aprender"
+
+Ejemplo 3:
+Input: "Curso de arrays"
+Output: "curso-de-arrays"
+*/
+
+const data =  "La forma de correr Python"
+/* const data = "La API para nunca parar de aprender"
+const data = "Curso de arrays"*/
+
+function joinString(data) {
+  const newData = data.toLowerCase()
+
+
+const divisiones = newData.split(" ", 7).join('-');
+
+console.log(divisiones);
+}
+
+joinString(data) 
