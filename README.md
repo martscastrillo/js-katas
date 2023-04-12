4069b0d7dfb57fb65be2b93d3fb3603a795fb5f4
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-12 10:37:37 +0200

    kata 37

--- README.md
+++ README.md
@@ -727,3 +727,46 @@ Output
 {"nombre":"Erik","instrumento":"Guitarra"},
 {"nombre":"Paul","instrumento":"Bateria"}
 ]}
+
+---
+
+- **KATA 37:**
+
+En este reto dada la siguiente clase:
+
+export class Course {
+constructor({
+name,
+classes = [],
+}) {
+this.name = name;
+this.classes = classes;
+}
+
+get name() {
+
+}
+
+set name(nuevoNombrecito) {
+
+}
+}
+
+Usando el set de la propiedad name valida siempre que se le quiera asignar un valor lo siguiente:
+
+Que sea de tipo string.
+Si es string convierte la primera letra de cada palabra del nombre en mayúsculas
+Si no es un string no se cambia el valor de la propiedad name.
+La solución debería tener un input y output como los siguientes:
+
+Input
+
+const courseName = "curso de programación básica"
+const nombreMaysuculas = new Course({
+name: courseName,
+})
+nombreMayusculas.name
+
+Output
+
+'Curso de Programación Básica'
---
+++ kata037.js
@@ -0,0 +1,65 @@
+/* En este reto dada la siguiente clase:
+
+export class Course {
+  constructor({
+    name,
+    classes = [],
+  }) {
+    this.name = name;
+    this.classes = classes;
+  }
+
+  get name() {
+
+  }
+
+  set name(nuevoNombrecito) {
+
+  }
+}
+
+Usando el set de la propiedad name valida siempre que se le quiera asignar un valor lo siguiente:
+
+Que sea de tipo string.
+Si es string convierte la primera letra de cada palabra del nombre en mayúsculas
+Si no es un string no se cambia el valor de la propiedad name.
+La solución debería tener un input y output como los siguientes:
+
+Input
+
+const courseName = "curso de programación básica"
+const nombreMaysuculas = new Course({
+    name: courseName,
+})
+nombreMayusculas.name
+
+Output
+
+'Curso de Programación Básica' */
+
+
+   class Course {
+    constructor({
+      name,
+      classes = [],
+    }) {
+      this.name = name;
+      this.classes = classes;
+    }
+  
+    get name() {
+      return  this._name;
+    }
+  
+    set name(nuevoNombrecito) {
+      this._name = nuevoNombrecito;
+    }
+  }
+
+  const courseName = "curso de programación básica"
+  const NewCourse = new Course({
+    name: courseName,
+})
+
+
+console.log(NewCourse.name);
