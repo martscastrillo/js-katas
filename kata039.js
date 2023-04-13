84bf0523d4262b4afb56e262227fd46a021141cd
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-13 16:12:06 +0200

    add kata 39

---
+++ kata039.js
@@ -0,0 +1,39 @@
+/* En este desafío tendrás que crear un closure que nos permita almacenar datos de mascotas en cualquier momento.
+
+Los datos pueden venir de distintas maneras, pueden ser objetos, strings o arrays. En el primer llamado a la función nos servirá para crear nuestra lista en un inicio mientras que los demás llamados nos ayudará a agregar elementos al final de la lista.
+
+Si en algún momento llamamos a la función sin pasarle ningún parámetro esta nos devolverá el array con los datos de todos las mascotas introducidas.
+
+Input:
+
+const myPetList = createPetList();
+
+myPetList("michi");
+
+myPetList("firulais");
+
+myPetList();
+
+Output:
+
+["michi", "firulais"]
+ */
+function createPetList() {
+  const mascotas = [];
+  return function listaMascotas(mascota) {
+    if (mascota) {
+      mascotas.push(mascota);
+      if(mascotas.length === 2){
+      console.log(mascotas);}
+    }
+    return mascotas;
+  } 
+  
+}
+const myPetList = createPetList();
+
+myPetList("michi");
+
+myPetList("firulais");
+
+myPetList();
