3e8547e83ae6d9ffe1cfe5d0e8ed58d71e0b3785
Marta Castrillo <mart.castrillo@gmail.com>
2023-05-26 14:42:12 +0200

    add katas subject

--- kata040.js
+++ kata040.js
@@ -1,6 +1,6 @@
 /* En este desafío tendrás que crear tu propia implementación del método shift.
 
-Contrario al reto anterior, aquí tendrás que hacer tu propio método shift para borrar el primer elemento que exista en tu array y retornar el elemento eliminado (no olvides modificar la longitud).
+Tendrás que hacer tu propio método shift para borrar el primer elemento que exista en tu array y retornar el elemento eliminado (no olvides modificar la longitud).
 
 En caso de ser un array vacío, este método deberá devolver undefined.
 
